f22500d5d06bc3cc12580059edd36b5d94c5553a
HoLamBaoKhuyen <holambaokhuyen@gmail.com>
2021-06-26 19:17:45 +0700

    create register activity

--- src/app/src/main/java/com/example/serenitea/RegisterActivity.java
+++ src/app/src/main/java/com/example/serenitea/RegisterActivity.java
@@ -4,10 +4,21 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
 /* Trang Đăng kí
@@ -16,15 +27,39 @@ public class RegisterActivity extends AppCompatActivity {
 * Chuyển sang trang Profile (SetupActivity) --> SendUserToSetupActivity()
 *
 * */
+    private EditText Username, UserPassword, UserConfirmPassword;
+    private Button SignUpButton;
+    private FirebaseAuth mAuth;
+    private ProgressDialog loadingBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        mAuth = FirebaseAuth.getInstance();
+
+        //event click Back
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("Back");
         actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#FFFFFF")));
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+        //get text from input
+        Username = (EditText) findViewById(R.id.editTxt_username);
+        UserPassword = (EditText) findViewById(R.id.editTxt_password);
+        UserConfirmPassword = (EditText) findViewById(R.id.editTxt_confirm);
+        SignUpButton = (Button) findViewById(R.id.btn_signup);
+        loadingBar = new ProgressDialog(this);
+
+        //event click SignUpButton
+        SignUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CreateNewAccount();
+            }
+        });
+
     }
 
     @Override
@@ -44,8 +79,47 @@ public class RegisterActivity extends AppCompatActivity {
         //kiểm tra nếu hiện tại user đã login rồi --> gọi SendUserToMainActivity()
     }
 
-    private void CreateNewAccount(){
-        //tạo tài khoản lưu trên FireBase
+    private void CreateNewAccount(){//tạo tài khoản lưu trên FireBase
+
+        String username = Username.getText().toString();
+        String pwd = UserPassword.getText().toString();
+        String confirmPwd = UserConfirmPassword.getText().toString();
+
+        //check nếu user chưa nhập field
+        if (TextUtils.isEmpty(username)){
+            Toast.makeText(this, "Please input your Username", Toast.LENGTH_SHORT).show();
+        }
+        else if (TextUtils.isEmpty(pwd)){
+            Toast.makeText(this, "Please input your Password", Toast.LENGTH_SHORT).show();
+        }
+        else if (TextUtils.isEmpty(confirmPwd)){
+            Toast.makeText(this, "Please input your Confirm Password", Toast.LENGTH_SHORT).show();
+        }
+        else if (!pwd.equals(confirmPwd)){
+            Toast.makeText(this, "Your password do not match with your confirm password", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            loadingBar.setTitle("Creating New Account");
+            loadingBar.setMessage("Please wait a moment...");
+            loadingBar.show();
+            loadingBar.setCanceledOnTouchOutside(true);
+
+            mAuth.createUserWithEmailAndPassword(username, pwd).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+                        Toast.makeText(RegisterActivity.this, "Create account successfully", Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        String message = task.getException().getMessage();
+                        Toast.makeText(RegisterActivity.this, "Error occured: " + message, Toast.LENGTH_SHORT).show();
+                    }
+                    loadingBar.dismiss();
+                }
+            });
+        }
+
+
     }
 
     private void SendUserToSetupActivity(){
