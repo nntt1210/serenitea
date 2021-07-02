17c7349b0f9c9ed3e8876e572dafe0e20ce17f29
nntt1210 <58521730+nntt1210@users.noreply.github.com>
2021-07-02 15:56:11 +0700

    add screen to view profile

--- src/app/src/main/java/com/example/serenitea/ProfileActivity.java
+++ src/app/src/main/java/com/example/serenitea/ProfileActivity.java
@@ -1,7 +1,10 @@
 package com.example.serenitea;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 public class ProfileActivity extends AppCompatActivity {
@@ -15,5 +18,11 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        //event click Back
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("");
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#FFFFFF")));
+        actionBar.setDisplayHomeAsUpEnabled(true);
     }
 }
