14622e3f6f4f62c6ccaa097deae9b46be698a775
HoLamBaoKhuyen <holambaokhuyen@gmail.com>
2021-07-02 10:08:02 +0700

    create a notification for reset password

--- src/app/src/main/java/com/example/serenitea/ForgotPasswordActivity.java
+++ src/app/src/main/java/com/example/serenitea/ForgotPasswordActivity.java
@@ -3,10 +3,15 @@ package com.example.serenitea;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -34,6 +39,14 @@ public class ForgotPasswordActivity extends AppCompatActivity {
         ResetEmailInput = (EditText) findViewById(R.id.forgot_email);
         ResetPasswordSendEmailButton = (Button) findViewById(R.id.btn_send);
 
+        //notification
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel("My Notification", "My Notification", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+
+
         //event click Back
         ActionBar actionBar = getSupportActionBar();
         actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#FFFFFF")));
@@ -45,27 +58,34 @@ public class ForgotPasswordActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String userEmail = ResetEmailInput.getText().toString();
 
-                if (TextUtils.isEmpty(userEmail)){
+                if (TextUtils.isEmpty(userEmail)) {
                     Toast.makeText(ForgotPasswordActivity.this, "Please write your valid email address first", Toast.LENGTH_SHORT).show();
-                }
-                else {
+                } else {
                     mAuth.sendPasswordResetEmail(userEmail).addOnCompleteListener(new OnCompleteListener<Void>() {
                         @Override
                         public void onComplete(@NonNull Task<Void> task) {
-                            if (task.isSuccessful()){
-                               Toast.makeText(ForgotPasswordActivity.this,"Please check your Email Account if you want to reset your password",Toast.LENGTH_SHORT).show();
-                               startActivity(new Intent(ForgotPasswordActivity.this, LoginActivity.class));
-                            }
-                            else{
+                            if (task.isSuccessful()) {
+                                Toast.makeText(ForgotPasswordActivity.this, "Please check your Email Account if you want to reset your password", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(ForgotPasswordActivity.this, LoginActivity.class));
+
+                                //notification
+                                NotificationCompat.Builder builder = new NotificationCompat.Builder(ForgotPasswordActivity.this,"khuyen")
+                                        .setSmallIcon(R.drawable.ic_dropdown_arrow)
+                                        .setContentTitle("Reset password for SERENITEA")
+                                        .setContentText("Please check your Email Account if you want to reset your password");
+
+                                NotificationManagerCompat managerCompat = NotificationManagerCompat.from(ForgotPasswordActivity.this);
+                                managerCompat.notify(1, builder.build());
+
+                            } else {
                                 String message = task.getException().getMessage();
-                                Toast.makeText(ForgotPasswordActivity.this,"Error: " + message, Toast.LENGTH_LONG).show();
+                                Toast.makeText(ForgotPasswordActivity.this, "Error: " + message, Toast.LENGTH_LONG).show();
                             }
                         }
                     });
                 }
             }
         });
-
 
 
     }
