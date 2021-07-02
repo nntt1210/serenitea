5eadb6d728f2fc1dfab77e3d07b249dc7b90da32
HoLamBaoKhuyen <holambaokhuyen@gmail.com>
2021-07-02 09:40:52 +0700

    Firebase send an email to confirm password

--- src/app/src/main/java/com/example/serenitea/ForgotPasswordActivity.java
+++ src/app/src/main/java/com/example/serenitea/ForgotPasswordActivity.java
@@ -1,22 +1,72 @@
 package com.example.serenitea;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ForgotPasswordActivity extends AppCompatActivity {
+
+    private EditText ResetEmailInput;
+    private Button ResetPasswordSendEmailButton;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
 
+        mAuth = FirebaseAuth.getInstance();
+
+        ResetEmailInput = (EditText) findViewById(R.id.forgot_email);
+        ResetPasswordSendEmailButton = (Button) findViewById(R.id.btn_send);
+
         //event click Back
         ActionBar actionBar = getSupportActionBar();
         actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#FFFFFF")));
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+        //event click Send Button
+        ResetPasswordSendEmailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userEmail = ResetEmailInput.getText().toString();
+
+                if (TextUtils.isEmpty(userEmail)){
+                    Toast.makeText(ForgotPasswordActivity.this, "Please write your valid email address first", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    mAuth.sendPasswordResetEmail(userEmail).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()){
+                               Toast.makeText(ForgotPasswordActivity.this,"Please check your Email Account if you want to reset your password",Toast.LENGTH_SHORT).show();
+                               startActivity(new Intent(ForgotPasswordActivity.this, LoginActivity.class));
+                            }
+                            else{
+                                String message = task.getException().getMessage();
+                                Toast.makeText(ForgotPasswordActivity.this,"Error: " + message, Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+
+
     }
 }
