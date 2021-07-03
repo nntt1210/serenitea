784b3daedaf34d13e48e5f4fa8776642485b64f8
nntt1210 <58521730+nntt1210@users.noreply.github.com>
2021-07-03 21:10:25 +0700

    add screen for changing password

---
+++ src/app/src/main/java/com/example/serenitea/ChangePasswordActivity.java
@@ -0,0 +1,24 @@
+package com.example.serenitea;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+
+public class ChangePasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_password);
+
+        //event click Back
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("");
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#FFFFFF")));
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+    }
+}
