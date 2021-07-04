f5c42cc0d27457ea3042e3096ca5b94fe9858582
nntt1210 <58521730+nntt1210@users.noreply.github.com>
2021-07-04 11:02:29 +0700

    use fragment instead of activity

--- src/app/src/main/java/com/example/serenitea/ChangePasswordActivity.java
+++ src/app/src/main/java/com/example/serenitea/ChangePasswordActivity.java
@@ -1,24 +1,22 @@
 package com.example.serenitea;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class ChangePasswordActivity extends AppCompatActivity {
+public class ChangePasswordActivity extends Fragment {
 
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_change_password);
-
-        //event click Back
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setTitle("");
-        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#FFFFFF")));
-        actionBar.setDisplayHomeAsUpEnabled(true);
-
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_change_password, container, false);
     }
 }
