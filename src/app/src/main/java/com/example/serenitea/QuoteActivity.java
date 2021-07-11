62a00be8b2d4dd5817aec5605d8e9200fce4706e
nntt1210 <58521730+nntt1210@users.noreply.github.com>
2021-07-11 21:52:57 +0700

    update layout

--- src/app/src/main/java/com/example/serenitea/QuoteActivity.java
+++ src/app/src/main/java/com/example/serenitea/QuoteActivity.java
@@ -1,9 +1,13 @@
 package com.example.serenitea;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import java.lang.Math;
@@ -32,9 +36,24 @@ public class QuoteActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quote);
 
-        QuoteView = (TextView)findViewById(R.id.textView);
+        //event click Back
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("");
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#FFFFFF")));
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        QuoteView = (TextView)findViewById(R.id.text_quote);
         GetEmotion();
         GenerateQuote();
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
     //id dissatisfied: 21 - 40, happy: 1 - 10; neutral: 41 - 50; satisfied: 61 - 70; very satisfied: 81 - 90
 
