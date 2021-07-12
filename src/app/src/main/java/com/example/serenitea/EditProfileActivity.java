764ad21cb27de88adcbd29f4ff2745144d6d6263
nntt1210 <58521730+nntt1210@users.noreply.github.com>
2021-07-12 08:30:49 +0700

    update back button of edit profile

--- src/app/src/main/java/com/example/serenitea/EditProfileActivity.java
+++ src/app/src/main/java/com/example/serenitea/EditProfileActivity.java
@@ -1,11 +1,14 @@
 package com.example.serenitea;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class EditProfileActivity extends AppCompatActivity {
     @Override
@@ -20,4 +23,16 @@ public class EditProfileActivity extends AppCompatActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Intent intent = new Intent(EditProfileActivity.this, SettingsActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
