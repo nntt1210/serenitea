abf70a78be5a60df992560340245cd307cdd4eed
nntt1210 <58521730+nntt1210@users.noreply.github.com>
2021-07-16 20:23:09 +0700

    update settings screen

--- src/app/src/main/java/com/example/serenitea/SettingsActivity.java
+++ src/app/src/main/java/com/example/serenitea/SettingsActivity.java
@@ -11,41 +11,44 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class SettingsActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
 //    private DrawerLayout drawer;
+    private TextView txtInfo, txtDob, txtCot;
     private ImageButton closeBtn, backBtn;
+    private String curUser;
+    private String name,dob,cot,gender;
+    private FirebaseAuth mAuth;
+    private DatabaseReference userRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-//        //event click Back
-//        ActionBar actionBar = getSupportActionBar();
-//        actionBar.setTitle("");
-//        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#FFFFFF")));
-//        actionBar.setDisplayHomeAsUpEnabled(true);
+        NavigationView navigationView = (NavigationView)findViewById(R.id.settings_nav_view);
+        View headerView = navigationView.getHeaderView(0);
+        txtInfo= headerView.findViewById(R.id.txt_info);
+        txtDob= headerView.findViewById(R.id.txt_dob);
+        txtCot= headerView.findViewById((R.id.txt_cup_of_tea));
 
-//        Toolbar toolbar = findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-//        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.white)));
-//        getSupportActionBar().setTitle("");
-
-//        drawer = findViewById(R.id.settings_layout);
-
-        NavigationView navigationView = findViewById(R.id.settings_nav_view);
+        mAuth = FirebaseAuth.getInstance();
         navigationView.setNavigationItemSelectedListener(this);
-//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
-//                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-//        drawer.addDrawerListener(toggle);
-//        toggle.syncState();
 
         if (savedInstanceState == null) {
 //            drawer.openDrawer(GravityCompat.START);
@@ -72,6 +75,49 @@ public class SettingsActivity extends AppCompatActivity implements NavigationVie
             }
         });
 
+    }
+
+    protected void setProfile()
+    {
+        curUser= mAuth.getCurrentUser().getUid();
+        userRef= FirebaseDatabase.getInstance().getReference().child("users").child(curUser);
+        userRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists())
+                {
+                    name = snapshot.child("nickname").getValue().toString();
+                    gender=snapshot.child("gender").getValue().toString();
+                    dob=snapshot.child("dob").getValue().toString();
+                    cot=snapshot.child("tea").getValue().toString();
+
+                    txtInfo.setText(name);
+                    txtDob.setText(dob);
+                    txtCot.setText(cot);
+
+
+                    if  (gender.equals("Female")){
+                        Drawable f = getResources().getDrawable(R.drawable.ic_women);
+                        txtInfo.setCompoundDrawablesWithIntrinsicBounds(f,null,null,null);
+                    }
+                    else if (gender.equals("Male")){
+                        Drawable m=getResources().getDrawable(R.drawable.ic_men);
+                        txtInfo.setCompoundDrawablesWithIntrinsicBounds(m,null,null,null);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        setProfile();
     }
 
     @Override
