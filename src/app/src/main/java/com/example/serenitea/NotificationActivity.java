fd50a8bc5ca1064047b76a0ff8d51f69f313803b
Tina Lam <holambaokhuyen@gmail.com>
2021-07-23 22:49:55 +0700

    change: notification get from DB

--- src/app/src/main/java/com/example/serenitea/NotificationActivity.java
+++ src/app/src/main/java/com/example/serenitea/NotificationActivity.java
@@ -19,37 +19,14 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class NotificationActivity extends AppCompatActivity {
-
-//    ArrayList<String> listNotification;
-//    NotificationAdapter notificationAdapter;
-//    ListView listViewNotification;
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_notification);
-//
-//        //event click Back
-//        ActionBar actionBar = getSupportActionBar();
-//        actionBar.setTitle("");
-//        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#FFFFFF")));
-//        actionBar.setDisplayHomeAsUpEnabled(true);
-//
-//        listNotification = new ArrayList<>();
-//        listNotification.add("Mint");
-//        listNotification.add("Ziem");
-//        listNotification.add("Dic");
-//        listNotification.add("Lisa");
-//
-//        notificationAdapter = new NotificationAdapter(listNotification);
-//
-//        listViewNotification = findViewById(R.id.list_notification);
-//        listViewNotification.setAdapter(notificationAdapter);
-//    }
-
+    
     private FirebaseAuth mAuth;
     private DatabaseReference RootRef, NotiRef, UserRef;
     String currentUserId;
@@ -64,7 +41,7 @@ public class NotificationActivity extends AppCompatActivity {
         currentUserId = mAuth.getCurrentUser().getUid();
 
         RootRef = FirebaseDatabase.getInstance().getReference();
-        NotiRef = RootRef.child("receiveQuotes").child(currentUserId);
+        NotiRef = RootRef.child("notification").child(currentUserId);
         UserRef = RootRef.child("users");
 
         //event click Back
@@ -90,37 +67,36 @@ public class NotificationActivity extends AppCompatActivity {
 
             @Override
             protected void onBindViewHolder(@NonNull NotificationActivity.NotiViewHolder holder, int position, Quote model) {
-                String sender_id = getRef(position).getKey();
+                String noti_id = getRef(position).getKey();
 
-//               //each user
-//                NotiRef.child(sender_id).addListenerForSingleValueEvent(new ValueEventListener() {
-//                    @Override
-//                    public void onDataChange(@NonNull DataSnapshot snapshot2) {
-//                        if (snapshot2.exists()) {
-//                            Toast.makeText(NotificationActivity.this, snapshot2.getValue().toString(), Toast.LENGTH_LONG).show();
-//                        }
-//                    }
-//                    @Override
-//                    public void onCancelled(@NonNull DatabaseError error) {
-//                    }
-//                });
+                //each user
+                NotiRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (snapshot.exists()) {
+                            String sender_id = snapshot.child(noti_id).child("from").getValue().toString();
+//                            Toast.makeText(NotificationActivity.this, snapshot.child(noti_id).child("quote").getValue().toString(), Toast.LENGTH_LONG).show();
+                            UserRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot snapshot2) {
+                                    if (snapshot2.exists()) {
+                                        final String sender_name = snapshot2.child(sender_id).child("nickname").getValue().toString();
+                                        final String avatar = snapshot2.child(sender_id).child("avatar").getValue().toString();
+                                        holder.setContent(sender_name);
+                                        holder.setAvatar(avatar);
+                                    }
+                                }
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError error) {
+                                }
+                            });
+                        }
+                    }
 
-//                UserRef.addListenerForSingleValueEvent(new ValueEventListener() {
-//                    @Override
-//                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-//                        if (snapshot.exists()) {
-//                            final String sender_name = snapshot.child(sender_id).child("nickname").getValue().toString();
-//                            final String avatar = snapshot.child(sender_id).child("avatar").getValue().toString();
-//                            holder.setContent(sender_name);
-//                            holder.setAvatar(avatar);
-//                        }
-//                    }
-//                    @Override
-//                    public void onCancelled(@NonNull DatabaseError error) {
-//
-//                    }
-//                });
-
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                    }
+                });
 
                 //click event on each quote
                 holder.itemView.setOnClickListener(new View.OnClickListener() {
@@ -132,7 +108,6 @@ public class NotificationActivity extends AppCompatActivity {
             }
 
             @NonNull
-
             @Override
             public NotificationActivity.NotiViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
                 View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.notification_view, parent, false);
@@ -140,11 +115,10 @@ public class NotificationActivity extends AppCompatActivity {
                 return notiHolder;
             }
         };
-        //Toast.makeText(SendQuoteActivity.this, "hello", Toast.LENGTH_LONG).show();
+
         notiList.setAdapter(firebaseRecyclerAdapter);
         firebaseRecyclerAdapter.startListening();
     }
-
 
     public class NotiViewHolder extends RecyclerView.ViewHolder {
         ImageView imageViewAvatar;
@@ -155,16 +129,13 @@ public class NotificationActivity extends AppCompatActivity {
             imageViewAvatar = itemView.findViewById(R.id.notification_avatar);
             textViewNotification = itemView.findViewById(R.id.notification_content);
         }
-
         public void setAvatar(String id) {
             int resource = getResources().getIdentifier(id, "drawable", getPackageName());
             imageViewAvatar.setImageResource(resource);
         }
-
         public void setContent(String content) {
             textViewNotification.setText(content + " sent you a quote.");
         }
-
     }
 
     @Override
