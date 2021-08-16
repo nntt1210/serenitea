37b899d412be094e6a7c300d38591bdf2509524c
nntt1210 <58521730+nntt1210@users.noreply.github.com>
2021-08-16 11:27:47 +0700

    fix duplicate and remove last friend

--- src/app/src/main/java/com/example/serenitea/Friend.java
+++ src/app/src/main/java/com/example/serenitea/Friend.java
@@ -16,4 +16,12 @@ public class Friend {
         this.gender = gender;
         this.id=id;
     }
+
+    public void setID(String id) {
+        this.id = id;
+    }
+
+    public String getID() {
+        return this.id;
+    }
 }
