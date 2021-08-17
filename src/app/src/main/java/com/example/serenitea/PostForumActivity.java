6f73e1968115cccb0821deaa29315a55b13c6e8d
nntt1210 <58521730+nntt1210@users.noreply.github.com>
2021-08-17 15:34:00 +0700

    initialize screen for post

---
+++ src/app/src/main/java/com/example/serenitea/PostForumActivity.java
@@ -0,0 +1,20 @@
+package com.example.serenitea;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PostForumActivity extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_post_forum, container, false);
+        return view;
+    }
+}
