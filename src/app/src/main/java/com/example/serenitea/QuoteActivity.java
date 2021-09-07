a5d7eef0782fd304a1cf3590681ca9c59ae1007c
Huyen178 <63285465+Huyen178@users.noreply.github.com>
2021-09-07 21:37:06 +0700

    change getemotion

--- src/app/src/main/java/com/example/serenitea/QuoteActivity.java
+++ src/app/src/main/java/com/example/serenitea/QuoteActivity.java
@@ -70,7 +70,7 @@ public class QuoteActivity extends AppCompatActivity {
 
         QuoteView = (TextView) findViewById(R.id.text_quote);
         QuoteView.setMovementMethod(new ScrollingMovementMethod());
-        GenerateQuoteID(GetEmotion());
+        GetEmotion();
         GenerateQuote();
 
         btnFavoriteClicked = new Boolean(false);
@@ -124,38 +124,31 @@ public class QuoteActivity extends AppCompatActivity {
     private int GetEmotion() {
         int emo = getIntent().getIntExtra("emotion", 0);
         return emo;
-    }
-
-    private void GenerateQuoteID (int emo)
-    {
-        if (QuoteID == null)
-        {
-            int qid;
-            switch (emo) {
-                case 1: // dissatisfied - sad
-                    qid = (int) (Math.random() * ((30 - 21) + 1)) + 21;
+        int qid;
+        switch (emo) {
+            case 1: // dissatisfied - sad
+                qid = (int) (Math.random() * ((30 - 21) + 1)) + 21;
+                QuoteID = "00" + qid;
+                break;
+            case 2: // happy
+                qid = (int) (Math.random() * ((10 - 1) + 1)) + 1;
+                if (qid == 10)
                     QuoteID = "00" + qid;
-                    break;
-                case 2: // happy
-                    qid = (int) (Math.random() * ((10 - 1) + 1)) + 1;
-                    if (qid == 10)
-                        QuoteID = "00" + qid;
-                    else
-                        QuoteID = "000" + qid;
-                    break;
-                case 3: // neutral
-                    qid = (int) (Math.random() * ((90 - 81) + 1)) + 81;
-                    QuoteID = "00" + qid;
-                    break;
-                case 4: // angry
-                    qid = (int) (Math.random() * ((71 - 61) + 1)) + 61;
-                    QuoteID = "00" + qid;
-                    break;
-                case 5: // nervous
-                    qid = (int) (Math.random() * ((60 - 41) + 1)) + 41;
-                    QuoteID = "00" + qid;
-                    break;
-            }
+                else
+                    QuoteID = "000" + qid;
+                break;
+            case 3: // neutral
+                qid = (int) (Math.random() * ((90 - 81) + 1)) + 81;
+                QuoteID = "00" + qid;
+                break;
+            case 4: // angry
+                qid = (int) (Math.random() * ((71 - 61) + 1)) + 61;
+                QuoteID = "00" + qid;
+                break;
+            case 5: // nervous
+                qid = (int) (Math.random() * ((60 - 41) + 1)) + 41;
+                QuoteID = "00" + qid;
+                break;
         }
     }
 
