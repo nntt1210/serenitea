54b3f12692756ce2a8ad277474ae05ca062c6a0c
Huyen178 <63285465+Huyen178@users.noreply.github.com>
2021-09-08 09:39:48 +0700

    format Emotion

--- src/app/src/main/java/com/example/serenitea/EmotionActivity.java
+++ src/app/src/main/java/com/example/serenitea/EmotionActivity.java
@@ -10,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -22,11 +23,11 @@ import org.jetbrains.annotations.NotNull;
 import java.util.Calendar;
 
 public class EmotionActivity extends Fragment {
-/* Trang chọn tâm trạng để generate quote
-* Một số hàm:
-* Chọn tâm trạng, dựa vào tâm trạng đó chọn ra 1 quote trong database (có thể trả về id của quote)
-* Chuyển trang sang QuoteActivity --> SendUserToQuoteActivity()
-* */
+    /* Trang chọn tâm trạng để generate quote
+     * Một số hàm:
+     * Chọn tâm trạng, dựa vào tâm trạng đó chọn ra 1 quote trong database (có thể trả về id của quote)
+     * Chuyển trang sang QuoteActivity --> SendUserToQuoteActivity()
+     * */
 
     private int emotion = 0;
     private DatabaseReference diaryRef;
@@ -65,8 +66,7 @@ public class EmotionActivity extends Fragment {
     }
 
     //id dissatisfied: 21 - 30, happy: 1 - 10; nervous: 41 - 60; angry: 61 - 71; neutral: 81 - 90
-    private void changebtnSad ()
-    {
+    private void changebtnSad() {
         emotion = 1;
         int qid;
         qid = (int) (Math.random() * ((30 - 21) + 1)) + 21;
@@ -74,8 +74,8 @@ public class EmotionActivity extends Fragment {
         updateEmo();
         SendUserToQuoteActivity();
     }
-    private void changebtnHappy ()
-    {
+
+    private void changebtnHappy() {
         emotion = 2;
         int qid;
         qid = (int) (Math.random() * ((10 - 1) + 1)) + 1;
@@ -87,8 +87,8 @@ public class EmotionActivity extends Fragment {
         updateCup();
         SendUserToQuoteActivity();
     }
-    private void changebtnNeutral ()
-    {
+
+    private void changebtnNeutral() {
         emotion = 3;
         int qid;
         qid = (int) (Math.random() * ((90 - 81) + 1)) + 81;
@@ -96,8 +96,8 @@ public class EmotionActivity extends Fragment {
         updateEmo();
         SendUserToQuoteActivity();
     }
-    private void changebtnAngry ()
-    {
+
+    private void changebtnAngry() {
         emotion = 4;
         int qid;
         qid = (int) (Math.random() * ((71 - 61) + 1)) + 61;
@@ -105,8 +105,8 @@ public class EmotionActivity extends Fragment {
         updateEmo();
         SendUserToQuoteActivity();
     }
-    private void changebtnWorried ()
-    {
+
+    private void changebtnWorried() {
         emotion = 5;
         int qid;
         qid = (int) (Math.random() * ((60 - 41) + 1)) + 41;
@@ -114,9 +114,9 @@ public class EmotionActivity extends Fragment {
         updateEmo();
         SendUserToQuoteActivity();
     }
-    private void updateCup ()
-    {
-        DatabaseReference cupRef = FirebaseDatabase.getInstance().getReference().child("users/"+curUser);
+
+    private void updateCup() {
+        DatabaseReference cupRef = FirebaseDatabase.getInstance().getReference().child("users/" + curUser);
         cupRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
@@ -132,38 +132,33 @@ public class EmotionActivity extends Fragment {
             }
         });
     }
-    private void updateEmo ()
-    {
+
+    private void updateEmo() {
         FirebaseAuth mAuth;
         Calendar c = Calendar.getInstance();
         int month = c.get(Calendar.MONTH) + 1;
         int day = c.get(Calendar.DAY_OF_MONTH);
         String m = String.valueOf(month);
         String d = String.valueOf(day);
-        mAuth=FirebaseAuth.getInstance();
+        mAuth = FirebaseAuth.getInstance();
         curUser = mAuth.getCurrentUser().getUid();
-        diaryRef = FirebaseDatabase.getInstance().getReference().child("diary/"+curUser+"/"+m+"/"+d);
+        diaryRef = FirebaseDatabase.getInstance().getReference().child("diary/" + curUser + "/" + m + "/" + d);
         diaryRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if(snapshot.exists())
-                {
+                if (snapshot.exists()) {
                     if (snapshot.hasChild(String.valueOf(emotion))) {
                         curEmo = snapshot.child(String.valueOf(emotion)).getValue().toString();
                         update = Integer.parseInt(curEmo);
                         update = update + 1;
-                    }
-                    else {
+                    } else {
                         update = 1;
                     }
                     diaryRef.child(String.valueOf(emotion)).setValue(update);
-                }
-                else
-                {
-                    for(int i = 0; i<5; i++)
-                    {
+                } else {
+                    for (int i = 0; i < 5; i++) {
                         update = 0;
-                        diaryRef.child(String.valueOf(i+1)).setValue(update);
+                        diaryRef.child(String.valueOf(i + 1)).setValue(update);
                     }
                     update = 1;
                     diaryRef.child(String.valueOf(emotion)).setValue(update);
@@ -177,15 +172,15 @@ public class EmotionActivity extends Fragment {
 
     }
 
-//    @Override
+    //    @Override
 //    protected void onStart() {
 //        super.onStart();
 //    }
-    private void SendUserToQuoteActivity (){
+    private void SendUserToQuoteActivity() {
         Intent quoteIntent = new Intent(EmotionActivity.this.getActivity(), QuoteActivity.class);
 //        quoteIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         quoteIntent.putExtra("quoteID", QuoteID);
         startActivity(quoteIntent);
- //       finish();
+        //       finish();
     }
 }
